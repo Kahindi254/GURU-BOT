7f5add17d6d5ed822b8feeeb2c30fd9dc4e35df4
Kahindi254 <164636975+Kahindi254@users.noreply.github.com>
2024-03-23 19:54:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254784326737', 'kahindi bot', true],
+  ['6281255369012', 'david', true], 
   [''] 
 ] //Number of owners
 
